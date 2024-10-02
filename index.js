81c962bfec79dc2239b4a736633563e0a4694beb
Rachit Goyal <goyalrachit2004@gmail.com>
2024-10-02 10:10:23 +0530

    added isabove18 function

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const age = 19
-const name = "Rachit Goyal"
+const name1 = "Rachit Goyal"
 const dob = '10/14/2004'
 
 
 function sayName() {
-    console.log("my name is" + name);
-    
+    console.log("my name is" + name1);    
+}
+
+function isabove18() {
+    return age>= 18
 }
 
